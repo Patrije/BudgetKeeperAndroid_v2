ae8018e8e23ae6506c1c9eafee8660ae01011f3d
PatryDud <34285215+PatryDud@users.noreply.github.com>
2018-12-11 21:02:28 +0100

    add packages

--- app/src/main/java/com/example/pati/retrofitappintro/MainActivity.java
+++ app/src/main/java/com/example/pati/retrofitappintro/MainActivity.java
@@ -6,9 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
